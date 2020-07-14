d67252bff670cd7151266769b9adc9a2cf26c1ce
Keshava Pranath K <kpranath@gmail.com>
2020-07-14 22:52:50 +0530

    added mongo db and connected

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const Joi = require('joi');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
+
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('Connected to mongodb...'))
+    .catch(err => console.error('Couldnot connect to mongodb...'));
 
 app.use(express.json());
 app.use('/api/genre', genres);

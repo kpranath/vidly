2d96bce437cf01d00658b80e24a0c3d2091f29e4
Keshava Pranath K <kpranath@gmail.com>
2020-07-19 00:15:47 +0530

    ojectID

---
+++ objectId.js
@@ -0,0 +1,18 @@
+// _id: 5f125a0c5af3e92e4c59c687
+
+// 24 character long. 12 bytes.
+//4bytes = timestamp
+//3bytes = machine identifier
+//2bytes = process identfier
+//3bytes = counter
+
+
+const mongoose = require('mongoose');
+
+const id = new mongoose.Types.ObjectId();
+console.log(id);
+
+console.log(id.getTimestamp());
+
+const isValid = mongoose.Types.ObjectId.isValid('1234');
+console.log(isValid);

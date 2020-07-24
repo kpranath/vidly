a8cbcc26777fd27a7c3e370f0ebcc7f1f945edf7
Keshava Pranath K <kpranath@gmail.com>
2020-07-24 10:17:20 +0530

    error fixed

--- middleware/async.js
+++ middleware/async.js
@@ -1,7 +1,7 @@
 module.exports = function (handler) {
     return async (req, res, next) => {
         try {
-            await handler();
+            await handler(req, res);
         }
         catch (ex) {
             next(ex);
